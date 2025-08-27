fd3c459388afd942d9afa49b648981918f8481a0
Fad <fadhilerdyaqasmal@gmail.com>
2025-08-28 00:16:28 +0700

    Step 9: Streaming

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -11,6 +11,8 @@ import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
@@ -18,6 +20,13 @@ export async function fetchRevenue() {
 
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    
+    console.log('Data fetch completed after 3 seconds.');
+ 
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
